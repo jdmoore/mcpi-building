7970e936356e3d7d5cfd05457f0626c1758e2e49
jdmoore <jdmoore1342@gmail.com>
2016-02-23 10:57:30 -0500

    Implement getWidth(), and TORCH, SMALL_FARM arguments for getBlueprint() in VillagePlan class.

--- VillageBuilding.java
+++ VillageBuilding.java
@@ -11,10 +11,6 @@ import com.nocturnalcoding.mcpi-building.VillagePlan;
 public class VillageBuilding extends Building {
 	
 
-	/*private Block[][][] blueprint;
-	private int height;
-	private int width;*/
-	
 	VillageBuilding(VillagePlan plan) {
 		super(plan.getBlueprint(), plan.getWidth());
 		System.out.println("Constructor: " + plan.toString());
--- VillagePlan.java
+++ VillagePlan.java
@@ -4,26 +4,96 @@ import pi.Block;
 
 	/*
 	 * Under Construction
+	 * 
+	 * Class containing village building blueprints 
+	 * from minecraft.gamepedia.com/Village/Blueprints
 	 */
+	 
 
 public enum VillagePlan {
+	
 	/*
-	Class containing village building blueprints 
-	from minecraft.gamepedia.com/Village/Blueprints
-	*/
+	 * SMALL_FARM does not generate a crop layer
+	 * LARGE_FARM is omitted because it can be formed from two overlapping SMALL_FARMs.
+	 * 
+	 * (Not Implemented) WELL should be called with at y = -11. given that y is the desired "ground level."
+	 */
 
-	BLACKSMITH, BUTCHER_SHOP, CHURCH, FARM, LAMP_POST, LARGE_HOUSE, LIBRARY, SMALL_HOUSE, WELL;
+	BLACKSMITH, BUTCHER_SHOP, CHURCH, LAMP_POST, LARGE_HOUSE, LIBRARY, SMALL_FARM, SMALL_HOUSE, WELL;
 
 	public Block[][][] getBlueprint() {
+		// TODO: Test
 		// TODO: fix ladder issue
+		// TODO: fix torch issue
+		// TODO: implement BLACKSMITH, BUTCHER_SHOP, CHURCH, FARM, LAMP_POST, LARGE_HOUSE, LIBRARY, and, WELL
 		switch (this) {
+			/*
+			case BLACKSMITH:
+				break;
+				
+			case BUTCHER_SHOP:
+				break;
+				
+			case CHURCH:
+				break;
+			*/	
+			case LAMP_POST:
+				// TODO: Test
+				System.out.println("Lamp Post Blueprint");
+				
+				Block[] airRow   = {Block.AIR, Block.AIR, Block.AIR};
+				Block[] postRow  = {Block.AIR, Block.FENCE, Block.AIR};
+				Block[] lampRow  = {Block.TORCH, Block.OBSIDIAN, Block.TORCH};
+				Block[] torchRow = {Block.AIR, Block.TORCH, Block.AIR};
+				
+				Block[][] postLayer = { airRow,
+										postRow,
+										airRow};
+				Block[][] lampLayer = { torchRow,
+										lampRow,
+										torchRow};
+				
+				Block[][][] blueprint = {postLayer,
+										postLayer,
+										postLayer,
+										lampLayer};
+				
+				return blueprint;
+			/*	
+			case LARGE_HOUSE:
+				break;
+				
+			case LIBRARY:
+				break;
+			*/	
+			case SMALL_FARM:
+				//TODO: Test
+				System.out.println("Small Farm Blueprint")
+				
+				Block[] woodRow = {Block.WOOD, Block.WOOD, Block.WOOD, Block.WOOD, Block.WOOD, Block.WOOD, Block.WOOD};
+				Block[] soilRow = {Block.WOOD, Block.DIRT, Block.DIRT, Block.WATER, Block.DIRT, Block.DIRT, Block.WOOD};
+				
+				Block[][] firstLayer = {woodRow,
+										soilRow,
+										soilRow,
+										soilRow,
+										soilRow,
+										soilRow,
+										soilRow,
+										soilRow,
+										woodRow};
+				
+				Block [][][] blueprint = {firstLayer};
+				
+				return blueprint;
+				
 			case SMALL_HOUSE:
 				System.out.println("Small House Blueprint");
 
 				Block[] cobblestoneRow       = {Block.COBBLESTONE, Block.COBBLESTONE, Block.COBBLESTONE,
 								Block.COBBLESTONE, Block.COBBLESTONE};
 
-								//withData(3) orients the stair towards the building (north)
+				// Block.STAIRS_COBBLESTONE.withData(3) orients the stair towards the building (north)
 				Block[] stairsRow            = {Block.AIR, Block.AIR, Block.STAIRS_COBBLESTONE.withData(3), 
 								Block.AIR, Block.AIR};
 
@@ -106,7 +176,10 @@ public enum VillagePlan {
 						fourthLayer, fifthLayer, sixthLayer};
 				
 				return blueprint;
-			
+				
+			/*case WELL:
+			 *	break;
+			 */
 			default: System.out.println("Invalid type: returned new Block[0][0][0]");
 				 return new Block[0][0][0];
 		}
@@ -114,7 +187,33 @@ public enum VillagePlan {
 
 	public int getWidth() {
 		switch (this) {
-			case SMALL_HOUSE: return 5;
+			case: BLACKSMITH:
+				return 10;
+			
+			case BUTCHER_SHOP:
+				return 9;
+			
+			case CHURCH:
+				return 5;
+			
+			case LAMP_POST:
+				return 3;
+				
+			case LARGE_HOUSE:
+				return 9;
+				
+			case LIBRARY:
+				return 9;
+				
+			case SMALL_FARM:
+				return 7;
+				
+			case SMALL_HOUSE: 
+				return 5;
+				
+			case WELL:
+				return 6;
+				
 			default: System.out.println("Invalid type: -1 returned");
 				return -1;
 		}
@@ -123,11 +222,35 @@ public enum VillagePlan {
 	public String toString() {
 	
 		switch (this) {
-			// case LARGE_HOUSE: return "LARGE_HOUSE";
-			// case LIBRARY: return "LIBRARY";
-			case SMALL_HOUSE: return "SMALL_HOUSE";
-			default: return "Invalid type";
-		
+			case BLACKSMITH:
+				return "BLACKSMITH";
+			
+			case BUTCHER_SHOP:
+				return "BUTCHER_SHOP";
+			
+			case CHURCH:
+				return "CHURCH";
+			
+			case LAMP_POST:
+				return "LAMP_POST";
+				
+			case LARGE_FARM:
+				return "LARGE_FARM";
+			
+			case LARGE_HOUSE:
+				return "LARGE_HOUSE";
+				
+			case LIBRARY:
+				return "LIBRARY";
+				
+			case SMALL_FARM:
+				return "SMALL_FARM";
+				
+			case SMALL_HOUSE: 
+				return "SMALL_HOUSE";
+				
+			default:
+				return "Invalid type";
 		}
 	
 	}
