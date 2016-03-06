81a8f2817f32900823a841868764deb78acc6a25
jdmoore <jdmoore1342@gmail.com>
2016-03-06 17:49:18 -0500

    Add command-line arguments to VillageTest

--- VillageTest.java
+++ VillageTest.java
@@ -1,3 +1,19 @@
+    /* Copyright (C) 2016 Joshua Moore
+     *
+
+     * Licensed under the Apache License, Version 2.0 (the "License");
+     * you may not use this file except in compliance with the License.
+     * You may obtain a copy of the License at
+     * 
+     *     http://www.apache.org/licenses/LICENSE-2.0
+
+     * Unless required by applicable law or agreed to in writing, software
+     * distributed under the License is distributed on an "AS IS" BASIS,
+     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     * See the License for the specific language governing permissions and
+     * limitations under the License.
+     */
+
 package com.nocturnalcoding.mcpi_building;
 
 import com.nocturnalcoding.mcpi_building.VillageBuilding;
@@ -11,6 +27,50 @@ public class VillageTest {
 	public static void main(String[] args) {
 		System.out.println("Main function");
 		
+		String[] arguments = args;
+		
+		VillagePlan plan;
+		int x;
+		int y;
+		int z;
+		
+		/*if (arguments.length < 4) {
+			System.out.println("Improper argument number");
+			
+		} */
+		
+		String planArg = arguments[0];
+		planArg = planArg.toLowerCase();
+		switch (planArg) {
+		    case "church":
+		    	plan = VillagePlan.CHURCH;
+		    	break;
+		    case "lamp_post":
+		    	plan = VillagePlan.LAMP_POST;
+		    	break;
+		    case "road":
+		    	plan = VillagePlan.ROAD;
+		    	break;
+		    case "small_farm":
+		    	plan = VillagePlan.SMALL_FARM;
+		    	break;
+		    case "small_house":
+		    	plan = VillagePlan.SMALL_HOUSE;
+		    	break;
+		    case "well":
+		    	plan = VillagePlan.WELL;
+		    	break;
+		}
+		x = (int) arguments[1];
+		y = (int) arguments[2];
+		z = (int) arguments[3];
+		
+		VillageBuilding building = new VillageBuilding(plan);
+		
+		building.build(Direction.NORTH, x, y, z);
+		
+		
+		/*
 		VillageBuilding lamp = new VillageBuilding(VillagePlan.LAMP_POST);
 		System.out.println("Height = " + lamp.getHeight());
 		System.out.println("Width = " + lamp.getWidth());
@@ -40,6 +100,6 @@ public class VillageTest {
 		System.out.println("Height = " + well.getHeight());
 		System.out.println("Width = " + well.getWidth());
 		well.build(Direction.NORTH, 0, -11, 10);
-
+		*/
 	}
 }
